5f60eae773732bbdaba5f567a533149ebf22c07e
Eric Chen <eric.chen@f5.com>
2021-02-04 14:08:39 -0500

    fake fw; client vpc

--- vpc/main.tf
+++ vpc/main.tf
@@ -7,6 +7,13 @@ resource "aws_ec2_transit_gateway" "tgw" {
   description = "${var.prefix}-tgw-tf"
 }
 
+resource "aws_ec2_transit_gateway_vpc_attachment" "vpc0" {
+  subnet_ids         = var.vpc0_subnet_ids
+  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+  transit_gateway_default_route_table_association = false
+  vpc_id             = var.vpc0
+}
+
 resource "aws_ec2_transit_gateway_vpc_attachment" "vpc1" {
   subnet_ids         = var.vpc1_subnet_ids
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
@@ -16,17 +23,74 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "vpc1" {
 resource "aws_ec2_transit_gateway_vpc_attachment" "vpc2" {
   subnet_ids         = var.vpc2_subnet_ids
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+  transit_gateway_default_route_table_association = false  
   vpc_id             = var.vpc2
+}
+
+ resource "aws_ec2_transit_gateway_route_table" "client" {
+   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+ }
+
+ resource "aws_ec2_transit_gateway_route_table" "workload" {
+   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+ }
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "client_to_peer" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc0.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.client.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "peer_to_client" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc1.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.client.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "workload_to_peer" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc2.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.workload.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "peer_to_workload" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc1.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.workload.id
+}
+
+ resource "aws_ec2_transit_gateway_route_table_association" "client_to_peer" {
+   transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.vpc0.id
+   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.client.id
+ }
+
+ resource "aws_ec2_transit_gateway_route_table_association" "workload_to_peer" {
+   transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.vpc2.id
+   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.workload.id
+ }
+
+# resource "aws_ec2_transit_gateway_route_table_association" "peer_to_client" {
+#   transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.vpc1.id
+#   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.peer.id
+# }
+
+
+resource "aws_route" "vpc0-external-route" {
+  route_table_id            = var.vpc0_external_rt
+  destination_cidr_block    = "10.0.0.0/8"
+  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
 }
 
 resource "aws_route" "vpc1-external-route" {
   route_table_id            = var.vpc1_external_rt
+  destination_cidr_block    = "10.0.0.0/8"
+  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+}
+
+resource "aws_route" "vpc1-internal-route" {
+  route_table_id            = var.vpc1_internal_rt
   destination_cidr_block    = "10.2.0.0/16"
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
 }
 
 resource "aws_route" "vpc2-external-route" {
   route_table_id            = var.vpc2_external_rt
-  destination_cidr_block    = "10.1.0.0/16"
+  destination_cidr_block    = "10.0.0.0/8"
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
 }
--- vpc/vars.tf
+++ vpc/vars.tf
@@ -1,8 +1,13 @@
+variable "vpc0" {}
 variable "vpc1" {}
 variable "vpc2" {}
+variable "vpc0_subnet_ids" {}
 variable "vpc1_subnet_ids" {}
 variable "vpc2_subnet_ids" {}
+variable "vpc0_external_rt" {}
 variable "vpc1_external_rt" {}
+variable "vpc1_internal_rt" {}
+
 variable "vpc2_external_rt" {}
 
 variable "prefix" {}
